e51898b57a2358d32f066535038adaa7e71016a8
Jung Jongmin <sdr2936@gmail.com>
2021-09-25 15:18:15 +0900

    Feat: Test Controller 작업 및 브랜치 develop

---
+++ src/main/kotlin/com/mashup/eclassserver/controller/TestController.kt
@@ -0,0 +1,12 @@
+package com.mashup.eclassserver.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController{
+    @GetMapping("/ping")
+    fun ping(): String {
+        return "pong"
+    }
+}

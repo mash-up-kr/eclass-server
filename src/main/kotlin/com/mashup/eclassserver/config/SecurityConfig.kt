e8f56cfbfc898d8a99019cd72aeb708dc34a574e
Jung Jongmin <sdr2936@gmail.com>
2021-09-25 15:36:18 +0900

    Feat: SecurityConfig 설정

---
+++ src/main/kotlin/com/mashup/eclassserver/config/SecurityConfig.kt
@@ -0,0 +1,14 @@
+package com.mashup.eclassserver.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+    override fun configure(security: HttpSecurity) {
+        security.httpBasic().disable()
+    }
+}

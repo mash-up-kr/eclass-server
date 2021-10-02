b5f3cde6f2fed90c36dae3dabf65a64645be8bfc
luffy.dmonkey <luffy.dmonkey@kakaocorp.com>
2021-10-02 20:30:22 +0900

    Set config files

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    runtimeOnly("mysql:mysql-connector-java:8.0.15")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")

2c8422804c5ecfd1affbaa7ea6d22798a536554a
luffy.dmonkey <luffy.dmonkey@kakaocorp.com>
2021-10-02 13:33:56 +0900

    의존 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    val kotlinVersion = "1.5.31"
     id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.spring") version "1.5.31"
-    kotlin("plugin.jpa") version "1.5.31"
+    id("org.asciidoctor.convert") version "1.5.12"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    kotlin("plugin.jpa") version kotlinVersion
+    kotlin("kapt") version kotlinVersion
 }
 
 group = "com.mashup"
@@ -20,23 +23,56 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.querydsl:querydsl-jpa:4.4.0")
+    kapt("com.querydsl:querydsl-apt:4.4.0:jpa")
+
     runtimeOnly("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
+
+    asciidoctor("org.springframework.restdocs:spring-restdocs-asciidoctor")
+    testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+tasks {
+    test {
+        useJUnitPlatform()
     }
-}
 
-tasks.withType<Test> {
-    useJUnitPlatform()
+    compileKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    val snippetsDir = file("build/generated-snippets")
+    asciidoctor {
+        inputs.dir(snippetsDir)
+        dependsOn("restDocsTest")
+    }
+
+    register("restDocs") {
+        dependsOn(asciidoctor)
+        doLast {
+            copy {
+                from(file("$buildDir/asciidoc/html5"))
+                into(file("docs"))
+            }
+        }
+    }
+
+    register<Test>("restDocsTest") {
+        useJUnitPlatform()
+        filter {
+            includeTestsMatching("*ControllerTest")
+        }
+    }
 }
 
 tasks.getByName<Jar>("jar") {

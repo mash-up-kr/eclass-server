6e06fbdf5115da73132c7cbf26a891e65bed05d9
Seunghyeon-Kim <ssseungzz7@gmail.com>
2021-10-03 04:08:31 +0900

    뱃지 조회 테스트 추가

---
+++ src/test/kotlin/com/mashup/eclassserver/controller/BadgeControllerTest.kt
@@ -0,0 +1,40 @@
+package com.mashup.eclassserver.controller
+
+import com.mashup.eclassserver.model.dto.BadgeResponseDto
+import com.mashup.eclassserver.model.entity.Badge
+import com.mashup.eclassserver.service.BadgeService
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.restdocs.headers.HeaderDocumentation
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(BadgeController::class)
+internal class BadgeControllerTest : AbstractTestRestDocs() {
+
+    @MockBean
+    lateinit var badgeService: BadgeService
+
+    @Test
+    fun getBadgeList() {
+
+        val badgeList: List<BadgeResponseDto> = listOf(
+                BadgeResponseDto.of(Badge(1L, "심술쟁이", "image-url-1")),
+                BadgeResponseDto.of(Badge(2L, "산책 오지게 했다", "image-url-2")))
+
+        given(badgeService.getBadgeList()).willReturn(badgeList)
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/list"))
+                .andDo(MockMvcResultHandlers.print())
+                .andDo(
+                        MockMvcRestDocumentation.document(
+                                "badge/{method}",
+                                HeaderDocumentation.responseHeaders()
+                        )
+                )
+    }
+}

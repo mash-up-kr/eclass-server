2b207e12354dba405525043f35147621fd53acac
Jung Jongmin <sdr2936@gmail.com>
2021-10-03 22:24:09 +0900

    Fix: some errors and dialect

--- src/main/kotlin/com/mashup/eclassserver/model/entity/AttachedSticker.kt
+++ src/main/kotlin/com/mashup/eclassserver/model/entity/AttachedSticker.kt
@@ -1,14 +1,23 @@
 package com.mashup.eclassserver.model.entity
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
 @Entity
 data class AttachedSticker(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val attachedStickerId: Long = 0,
-    val imageUrl: String,
-    val name: String
+    val stickerId: Long,
+    val attachedId: Long,
+    val memberId: Long,
+    @Column(name ="sticker_x")
+    val stickerX: Double,
+    @Column(name ="sticker_y")
+    val stickerY: Double,
+    @Enumerated(EnumType.STRING)
+    val attachedType: AttachedType
 ) : BaseEntity()
+
+enum class AttachedType {
+    COVER,
+    DIARY
+}
